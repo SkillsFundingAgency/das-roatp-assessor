bcd53f666523d5f774461b77d4836f05d41d113c
Martin Lush <martin-hornsby@ntlworld.com>
2020-04-21 08:41:29 +0100

    Added mocked User for User Authentication

---
+++ src/SFA.DAS.RoatpAssessor/SFA.DAS.RoatpAssessor.Web.UnitTests/MockedObjects/MockedUser.cs
@@ -0,0 +1,34 @@
+﻿using SFA.DAS.RoatpAssessor.Web.Domain;
+using System.Collections.Generic;
+using System.Security.Claims;
+
+namespace SFA.DAS.RoatpAssessor.Web.UnitTests.MockedObjects
+{
+    public class MockedUser
+    {
+        private const string GivenName = "Test";
+        private const string Surname = "User";
+        private const string Email = "Test.User@example.com";
+
+        public static ClaimsPrincipal Setup(params string[] roles)
+        {
+            var claims = new List<Claim>
+            {
+                new Claim(ClaimTypes.GivenName, GivenName),
+                new Claim(ClaimTypes.Surname, Surname),
+                new Claim(ClaimTypes.Name, $"{GivenName} {Surname}"),
+                new Claim(ClaimTypes.Email, Email),
+                new Claim(ClaimTypes.Upn, Email)
+            };
+
+            foreach(var role in roles)
+            {
+                var rolesClaim = new Claim(Roles.RoleClaimType, role);
+                claims.Add(rolesClaim);
+            }
+
+            var user = new ClaimsPrincipal(new ClaimsIdentity(claims, "mock", ClaimTypes.Name, Roles.RoleClaimType));
+            return user;
+        }
+    }
+}
