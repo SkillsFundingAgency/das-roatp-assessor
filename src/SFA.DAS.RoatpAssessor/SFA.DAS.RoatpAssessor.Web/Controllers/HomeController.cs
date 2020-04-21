c88bfa111fc5bd2fa5c910efbf6570b90ce89963
Michael Young <mick.young1981@googlemail.com>
2020-04-21 09:07:23 +0100

    Added error page.

---
+++ src/SFA.DAS.RoatpAssessor/SFA.DAS.RoatpAssessor.Web/Controllers/HomeController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SFA.DAS.RoatpAssessor.Web.ViewModels;
+
+namespace SFA.DAS.RoatpAssessor.Web.Controllers
+{
+    public class HomeController : Controller
+    {
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = HttpContext.TraceIdentifier });
+        }
+    }
+}
