775347bb7dc6817ca012ca9207065b8615d4ec09
Michael Young <mick.young1981@googlemail.com>
2020-04-21 09:15:12 +0100

    Add ErrorPages

---
+++ src/SFA.DAS.RoatpAssessor/SFA.DAS.RoatpAssessor.Web/Controllers/ErrorPageController.cs
@@ -0,0 +1,38 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SFA.DAS.RoatpAssessor.Web.Controllers
+{
+    public class ErrorPageController : Controller
+    {
+        [Route("ErrorPage/404")]
+        public async Task<IActionResult> PageNotFound()
+        {
+            return View("~/Views/ErrorPage/PageNotFound.cshtml");
+        }
+
+        [Route("ErrorPage/500")]
+        public async Task<IActionResult> ServiceErrorHandler()
+        {
+            return RedirectToAction("ServiceError");
+        }
+
+        [Route("problem-with-service")]
+        public async Task<IActionResult> ServiceError()
+        {
+            return View("~/Views/ErrorPage/ServiceError.cshtml");
+        }
+
+        [Route("ErrorPage/503")]
+        public async Task<IActionResult> ServiceUnavailableHandler()
+        {
+            return RedirectToAction("ServiceUnavailable");
+        }
+
+        [Route("service-unavailable")]
+        public async Task<IActionResult> ServiceUnavailable()
+        {
+            return View("~/Views/ErrorPage/ServiceUnavailable.cshtml");
+        }
+    }
+}
