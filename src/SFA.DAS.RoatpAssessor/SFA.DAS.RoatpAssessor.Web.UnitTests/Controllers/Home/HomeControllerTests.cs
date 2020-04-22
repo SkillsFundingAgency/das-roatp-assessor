22de1dcdfbd02c96309033a3f33397152d14a3dc
Martin Lush <martin-hornsby@ntlworld.com>
2020-04-22 08:25:42 +0100

    Added unit test for HomeController

---
+++ src/SFA.DAS.RoatpAssessor/SFA.DAS.RoatpAssessor.Web.UnitTests/Controllers/Home/HomeControllerTests.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.VisualStudio.TestPlatform.ObjectModel.DataCollection;
+using NUnit.Framework;
+using SFA.DAS.RoatpAssessor.Web.Controllers;
+using SFA.DAS.RoatpAssessor.Web.UnitTests.MockedObjects;
+using SFA.DAS.RoatpAssessor.Web.ViewModels;
+
+namespace SFA.DAS.RoatpAssessor.Web.UnitTests.Controllers.Home
+{
+    [TestFixture]
+    public class HomeControllerTests
+    {
+        private HomeController _controller;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _controller = new HomeController()
+            {
+                ControllerContext = MockedControllerContext.Setup()
+            };
+        }
+
+        [Test]
+        public void Error_returns_view_with_expected_viewmodel()
+        {
+            var expectedViewModel = new ErrorViewModel { RequestId = _controller.HttpContext.TraceIdentifier };
+
+            var result = _controller.Error() as ViewResult;
+            var actualViewModel = result?.Model as ErrorViewModel;
+
+            Assert.That(result, Is.Not.Null);
+            Assert.That(actualViewModel, Is.Not.Null);           
+            Assert.That(actualViewModel.RequestId, Is.EqualTo(expectedViewModel.RequestId));
+        }
+    }
+}
