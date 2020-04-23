356c9be59381342f23e61d9277ef27bb4e12d9bb
Martin Lush <martin-hornsby@ntlworld.com>
2020-04-23 14:49:58 +0100

    APR-1490 - Added controller & views to show Application Overview

--- src/SFA.DAS.RoatpAssessor/SFA.DAS.RoatpAssessor.Web/Controllers/HomeController.cs
+++ src/SFA.DAS.RoatpAssessor/SFA.DAS.RoatpAssessor.Web/Controllers/HomeController.cs
@@ -1,10 +1,23 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using SFA.DAS.RoatpAssessor.Web.ApplyTypes;
+using SFA.DAS.RoatpAssessor.Web.ApplyTypes.Apply;
+using SFA.DAS.RoatpAssessor.Web.Domain;
+using SFA.DAS.RoatpAssessor.Web.Services;
 using SFA.DAS.RoatpAssessor.Web.ViewModels;
+using System;
+using System.Threading.Tasks;
 
 namespace SFA.DAS.RoatpAssessor.Web.Controllers
 {
     public class HomeController : Controller
     {
+        private readonly IAssessorOverviewOrchestrator _overviewOrchestrator;
+
+        public HomeController(IAssessorOverviewOrchestrator overviewOrchestrator)
+        {
+            _overviewOrchestrator = overviewOrchestrator;
+        }
+
         public IActionResult Index()
         {
             return View();
@@ -14,6 +27,32 @@ namespace SFA.DAS.RoatpAssessor.Web.Controllers
         public IActionResult Error()
         {
             return View(new ErrorViewModel { RequestId = HttpContext.TraceIdentifier });
+        }
+
+        [HttpGet("Home/{applicationId}")]
+        public async Task<IActionResult> ViewApplication(Guid applicationId)
+        {
+            var username = User.UserDisplayName();
+
+            var viewModel = await _overviewOrchestrator.GetOverviewViewModel(new GetApplicationOverviewRequest(applicationId, username));
+
+            if (viewModel is null)
+            {
+                return RedirectToAction(nameof(Index));
+            }
+
+            switch (viewModel.AssessorReviewStatus)
+            {
+                case AssessorReviewStatus.New:
+                case AssessorReviewStatus.InProgress:
+                    return View("~/Views/Home/Application.cshtml", viewModel);
+                case AssessorReviewStatus.Approved:
+                case AssessorReviewStatus.Declined:
+                    return View("~/Views/Home/Application_ReadOnly.cshtml", viewModel);
+                default:
+                    return RedirectToAction(nameof(Index));
+            }
+
         }
     }
 }
