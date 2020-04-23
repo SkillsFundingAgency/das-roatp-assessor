cbd68f1beab10d9a9b25fbf5dd1cd349ed269d6f
Martin Lush <martin-hornsby@ntlworld.com>
2020-04-23 16:10:05 +0100

    APR-1490 - Refactored code to take in new GetAssessorSectionAnswers end point

--- src/SFA.DAS.RoatpAssessor/SFA.DAS.RoatpAssessor.Web/Services/AssessorOverviewOrchestrator.cs
+++ src/SFA.DAS.RoatpAssessor/SFA.DAS.RoatpAssessor.Web/Services/AssessorOverviewOrchestrator.cs
@@ -30,9 +30,37 @@ namespace SFA.DAS.RoatpAssessor.Web.Services
             }
 
             var viewmodel = new AssessorApplicationViewModel(application);
+            viewmodel.Sequences = GetCoreAssessorSequences();
 
-            #region Sequences Stubbed Data
-            viewmodel.Sequences = new List<AssessorSequence>
+            var savedStatuses = await _applyApiClient.GetAssessorSectionAnswers(application.ApplicationId);
+            if (savedStatuses is null || !savedStatuses.Any())
+            {
+                viewmodel.IsReadyForModeration = false;
+            }
+            else
+            {
+                // Inject the statuses into viewmodel
+                foreach (var currentStatus in savedStatuses)
+                {
+                    var sequence = viewmodel.Sequences.FirstOrDefault(seq => seq.SequenceNumber == currentStatus.SequenceNumber);
+                    var section = sequence?.Sections.FirstOrDefault(sec => sec.SectionNumber == currentStatus.SectionNumber);
+
+                    if(section != null)
+                    {
+                        section.Status = currentStatus.Status;
+                        section.StatusDescription = currentStatus.StatusDescription ?? currentStatus.Status;
+                    }
+                }
+
+                viewmodel.IsReadyForModeration = IsReadyForModeration(viewmodel);
+            }
+
+            return viewmodel;
+        }
+
+        private List<AssessorSequence> GetCoreAssessorSequences()
+        {
+            return new List<AssessorSequence>
             {
                 new AssessorSequence
                 {
@@ -41,11 +69,11 @@ namespace SFA.DAS.RoatpAssessor.Web.Services
                     SequenceTitle = "Protecting your apprentices checks",
                     Sections = new List<AssessorSection>
                     {
-                        new AssessorSection { SectionNumber = 2, DisplaySectionNumber = 1, LinkTitle = "Continuity plan for apprenticeship training", Status = SectionReviewStatus.New, StatusDescription = SectionReviewStatus.New },
-                        new AssessorSection { SectionNumber = 3, DisplaySectionNumber = 2, LinkTitle = "Equality and diversity policy", Status = SectionReviewStatus.New, StatusDescription = SectionReviewStatus.New },
-                        new AssessorSection { SectionNumber = 4, DisplaySectionNumber = 3, LinkTitle = "Safeguarding and Prevent duty policy", Status = SectionReviewStatus.New, StatusDescription = SectionReviewStatus.New },
-                        new AssessorSection { SectionNumber = 5, DisplaySectionNumber = 4, LinkTitle = "Health and safety policy", Status = SectionReviewStatus.New, StatusDescription = SectionReviewStatus.New },
-                        new AssessorSection { SectionNumber = 6, DisplaySectionNumber = 5, LinkTitle = "Acting as a subcontractor", Status = SectionReviewStatus.New, StatusDescription = SectionReviewStatus.New }
+                        new AssessorSection { SectionNumber = 2, DisplaySectionNumber = 1, LinkTitle = "Continuity plan for apprenticeship training", Status = "", StatusDescription = "" },
+                        new AssessorSection { SectionNumber = 3, DisplaySectionNumber = 2, LinkTitle = "Equality and diversity policy", Status = "", StatusDescription = "" },
+                        new AssessorSection { SectionNumber = 4, DisplaySectionNumber = 3, LinkTitle = "Safeguarding and Prevent duty policy", Status = "", StatusDescription = "" },
+                        new AssessorSection { SectionNumber = 5, DisplaySectionNumber = 4, LinkTitle = "Health and safety policy", Status = "", StatusDescription = "" },
+                        new AssessorSection { SectionNumber = 6, DisplaySectionNumber = 5, LinkTitle = "Acting as a subcontractor", Status = "", StatusDescription = "" }
                     }
                 },
 
@@ -56,12 +84,12 @@ namespace SFA.DAS.RoatpAssessor.Web.Services
                     SequenceTitle = "Readiness to engage checks",
                     Sections = new List<AssessorSection>
                     {
-                        new AssessorSection { SectionNumber = 2, DisplaySectionNumber = 1, LinkTitle = "Engaging with employers", Status = SectionReviewStatus.New, StatusDescription = SectionReviewStatus.New },
-                        new AssessorSection { SectionNumber = 3, DisplaySectionNumber = 2, LinkTitle = "Complaints policy", Status = SectionReviewStatus.New, StatusDescription = SectionReviewStatus.New },
-                        new AssessorSection { SectionNumber = 4, DisplaySectionNumber = 3, LinkTitle = "Contract for services template with employers", Status = SectionReviewStatus.New, StatusDescription = SectionReviewStatus.New },
-                        new AssessorSection { SectionNumber = 5, DisplaySectionNumber = 4, LinkTitle = "Commitment statement template", Status = SectionReviewStatus.New, StatusDescription = SectionReviewStatus.New },
-                        new AssessorSection { SectionNumber = 6, DisplaySectionNumber = 5, LinkTitle = "Prior learning of apprentices", Status = SectionReviewStatus.New, StatusDescription = SectionReviewStatus.New },
-                        new AssessorSection { SectionNumber = 7, DisplaySectionNumber = 6, LinkTitle = "Working with subcontractors", Status = SectionReviewStatus.New, StatusDescription = SectionReviewStatus.New }
+                        new AssessorSection { SectionNumber = 2, DisplaySectionNumber = 1, LinkTitle = "Engaging with employers", Status = "", StatusDescription = "" },
+                        new AssessorSection { SectionNumber = 3, DisplaySectionNumber = 2, LinkTitle = "Complaints policy", Status = "", StatusDescription = "" },
+                        new AssessorSection { SectionNumber = 4, DisplaySectionNumber = 3, LinkTitle = "Contract for services template with employers", Status = "", StatusDescription = "" },
+                        new AssessorSection { SectionNumber = 5, DisplaySectionNumber = 4, LinkTitle = "Commitment statement template", Status = "", StatusDescription = "" },
+                        new AssessorSection { SectionNumber = 6, DisplaySectionNumber = 5, LinkTitle = "Prior learning of apprentices", Status = "", StatusDescription = "" },
+                        new AssessorSection { SectionNumber = 7, DisplaySectionNumber = 6, LinkTitle = "Working with subcontractors", Status = "", StatusDescription = "" }
                     }
                 },
 
@@ -72,11 +100,11 @@ namespace SFA.DAS.RoatpAssessor.Web.Services
                     SequenceTitle = "Planning apprenticeship training checks",
                     Sections = new List<AssessorSection>
                     {
-                        new AssessorSection { SectionNumber = 2, DisplaySectionNumber = 1, LinkTitle = "Type of apprenticeship training", Status = SectionReviewStatus.New, StatusDescription = SectionReviewStatus.New },
-                        new AssessorSection { SectionNumber = 3, DisplaySectionNumber = 2, LinkTitle = "Supporting apprentices", Status = SectionReviewStatus.New, StatusDescription = SectionReviewStatus.New },
-                        new AssessorSection { SectionNumber = 4, DisplaySectionNumber = 3, LinkTitle = "Forecasting starts", Status = SectionReviewStatus.New, StatusDescription = SectionReviewStatus.New },
-                        new AssessorSection { SectionNumber = 5, DisplaySectionNumber = 4, LinkTitle = "Off the job training", Status = SectionReviewStatus.New, StatusDescription = SectionReviewStatus.New },
-                        new AssessorSection { SectionNumber = 6, DisplaySectionNumber = 5, LinkTitle = "Where apprentices will be trained", Status = SectionReviewStatus.New, StatusDescription = SectionReviewStatus.New }
+                        new AssessorSection { SectionNumber = 2, DisplaySectionNumber = 1, LinkTitle = "Type of apprenticeship training", Status = "", StatusDescription = "" },
+                        new AssessorSection { SectionNumber = 3, DisplaySectionNumber = 2, LinkTitle = "Supporting apprentices", Status = "", StatusDescription = "" },
+                        new AssessorSection { SectionNumber = 4, DisplaySectionNumber = 3, LinkTitle = "Forecasting starts", Status = "", StatusDescription = "" },
+                        new AssessorSection { SectionNumber = 5, DisplaySectionNumber = 4, LinkTitle = "Off the job training", Status = "", StatusDescription = "" },
+                        new AssessorSection { SectionNumber = 6, DisplaySectionNumber = 5, LinkTitle = "Where apprentices will be trained", Status = "", StatusDescription = "" }
                     }
                 },
 
@@ -87,12 +115,12 @@ namespace SFA.DAS.RoatpAssessor.Web.Services
                     SequenceTitle = "Delivering apprenticeship training checks",
                     Sections = new List<AssessorSection>
                     {
-                        new AssessorSection { SectionNumber = 2, DisplaySectionNumber = 1, LinkTitle = "Overall accountability for apprenticeships", Status = SectionReviewStatus.New, StatusDescription = SectionReviewStatus.New },
-                        new AssessorSection { SectionNumber = 3, DisplaySectionNumber = 2, LinkTitle = "Management hierarchy for apprenticeships", Status = SectionReviewStatus.New, StatusDescription = SectionReviewStatus.New },
-                        new AssessorSection { SectionNumber = 4, DisplaySectionNumber = 3, LinkTitle = "Quality and high standards in apprenticeship training", Status = SectionReviewStatus.New, StatusDescription = SectionReviewStatus.New },
-                        new AssessorSection { SectionNumber = 5, DisplaySectionNumber = 4, LinkTitle = "Developing and delivering training", Status = SectionReviewStatus.New, StatusDescription = SectionReviewStatus.New },
-                        new AssessorSection { SectionNumber = 6, DisplaySectionNumber = 5, LinkTitle = "Sectors and employee experience", Status = SectionReviewStatus.New, StatusDescription = SectionReviewStatus.New },
-                        new AssessorSection { SectionNumber = 7, DisplaySectionNumber = 6, LinkTitle = "Policy for professional development of employees", Status = SectionReviewStatus.New, StatusDescription = SectionReviewStatus.New }
+                        new AssessorSection { SectionNumber = 2, DisplaySectionNumber = 1, LinkTitle = "Overall accountability for apprenticeships", Status = "", StatusDescription = "" },
+                        new AssessorSection { SectionNumber = 3, DisplaySectionNumber = 2, LinkTitle = "Management hierarchy for apprenticeships", Status = "", StatusDescription = "" },
+                        new AssessorSection { SectionNumber = 4, DisplaySectionNumber = 3, LinkTitle = "Quality and high standards in apprenticeship training", Status = "", StatusDescription = "" },
+                        new AssessorSection { SectionNumber = 5, DisplaySectionNumber = 4, LinkTitle = "Developing and delivering training", Status = "", StatusDescription = "" },
+                        new AssessorSection { SectionNumber = 6, DisplaySectionNumber = 5, LinkTitle = "Sectors and employee experience", Status = "", StatusDescription = "" },
+                        new AssessorSection { SectionNumber = 7, DisplaySectionNumber = 6, LinkTitle = "Policy for professional development of employees", Status = "", StatusDescription = "" }
                     }
                 },
 
@@ -103,22 +131,17 @@ namespace SFA.DAS.RoatpAssessor.Web.Services
                     SequenceTitle = "Evaluating apprenticeship training checks",
                     Sections = new List<AssessorSection>
                     {
-                        new AssessorSection { SectionNumber = 2, DisplaySectionNumber = 1, LinkTitle = "Process for evaluating the quality of training delivered", Status = SectionReviewStatus.New, StatusDescription = SectionReviewStatus.New },
-                        new AssessorSection { SectionNumber = 3, DisplaySectionNumber = 2, LinkTitle = "Evaluating the quality of apprenticeship training", Status = SectionReviewStatus.New, StatusDescription = SectionReviewStatus.New },
-                        new AssessorSection { SectionNumber = 4, DisplaySectionNumber = 3, LinkTitle = "Systems and processes to collect apprenticeship data", Status = SectionReviewStatus.New, StatusDescription = SectionReviewStatus.New }
+                        new AssessorSection { SectionNumber = 2, DisplaySectionNumber = 1, LinkTitle = "Process for evaluating the quality of training delivered", Status = "", StatusDescription = "" },
+                        new AssessorSection { SectionNumber = 3, DisplaySectionNumber = 2, LinkTitle = "Evaluating the quality of apprenticeship training", Status = "", StatusDescription = "" },
+                        new AssessorSection { SectionNumber = 4, DisplaySectionNumber = 3, LinkTitle = "Systems and processes to collect apprenticeship data", Status = "", StatusDescription = "" }
                     }
                 },
             };
-            #endregion
-
-            viewmodel.ReadyToConfirm = CheckIsItReadyToConfirm(viewmodel);
-
-            return viewmodel;
         }
 
-        public bool CheckIsItReadyToConfirm(AssessorApplicationViewModel viewmodel)
+        public bool IsReadyForModeration(AssessorApplicationViewModel viewmodel)
         {
-            var isReadyToConfirm = true;
+            var isReadyForModeration = true;
 
             foreach (var sequence in viewmodel.Sequences)
             {
@@ -126,13 +149,13 @@ namespace SFA.DAS.RoatpAssessor.Web.Services
                 {
                     if (section.Status == null || (!section.Status.Equals(SectionReviewStatus.Pass) && !section.Status.Equals(SectionReviewStatus.Fail) && !section.Status.Equals(SectionReviewStatus.NotRequired)))
                     {
-                        isReadyToConfirm = false;
+                        isReadyForModeration = false;
                         break;
                     }
                 }
             }
 
-            return isReadyToConfirm;
+            return isReadyForModeration;
         }
     }
 }
