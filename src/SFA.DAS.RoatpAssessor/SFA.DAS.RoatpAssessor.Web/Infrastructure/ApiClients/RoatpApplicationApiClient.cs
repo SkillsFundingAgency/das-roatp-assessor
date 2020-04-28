b6377e83b43e5a8b39a9d73d1b255fe9e65f5075
Martin Lush <martin-hornsby@ntlworld.com>
2020-04-28 11:07:19 +0100

    APR-1490 - Removed StatusDescription

--- src/SFA.DAS.RoatpAssessor/SFA.DAS.RoatpAssessor.Web/Infrastructure/ApiClients/RoatpApplicationApiClient.cs
+++ src/SFA.DAS.RoatpAssessor/SFA.DAS.RoatpAssessor.Web/Infrastructure/ApiClients/RoatpApplicationApiClient.cs
@@ -105,11 +105,11 @@ namespace SFA.DAS.RoatpAssessor.Web.Infrastructure.ApiClients
         {
             var answers = new List<dynamic>
             {
-                new { SequenceNumber = 4, SectionNumber = 2, Status = "Fail", StatusDescription = "Fail" },
-                new { SequenceNumber = 4, SectionNumber = 3, Status = "In progress", StatusDescription = "In progress" },
-                new { SequenceNumber = 4, SectionNumber = 4, Status = "Fail", StatusDescription = "2 Fails out of 4" },
-                new { SequenceNumber = 4, SectionNumber = 5, Status = "Pass", StatusDescription = "Pass" },
-                new { SequenceNumber = 5, SectionNumber = 2, Status = "Not required", StatusDescription = "Not required" }
+                new { SequenceNumber = 4, SectionNumber = 2, Status = "Fail" },
+                new { SequenceNumber = 4, SectionNumber = 3, Status = "In progress" },
+                new { SequenceNumber = 4, SectionNumber = 4, Status = "2 Fails out of 4" },
+                new { SequenceNumber = 4, SectionNumber = 5, Status = "Pass" },
+                new { SequenceNumber = 5, SectionNumber = 2, Status = "Not required" }
             };
 
             return await Task.FromResult(answers);
