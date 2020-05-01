8bab16afa84a9e8882827a9dead147096973d098
Martin Lush <martin-hornsby@ntlworld.com>
2020-05-01 11:44:42 +0100

    APR-1656 - Removed commented out code

--- src/SFA.DAS.RoatpAssessor/SFA.DAS.RoatpAssessor.Web/ViewModels/ReviewAnswersViewModel.cs
+++ src/SFA.DAS.RoatpAssessor/SFA.DAS.RoatpAssessor.Web/ViewModels/ReviewAnswersViewModel.cs
@@ -48,26 +48,4 @@ namespace SFA.DAS.RoatpAssessor.Web.ViewModels
             }
         }
     }
-
-
-    //public class ValidationErrorDetail
-    //{
-    //    public ValidationErrorDetail()
-    //    {
-    //    }
-
-    //    public ValidationErrorDetail(string field, string errorMessage)
-    //    {
-    //        Field = field;
-    //        ErrorMessage = errorMessage;
-    //    }
-
-    //    public ValidationErrorDetail(string errorMessage)
-    //    {
-    //        ErrorMessage = errorMessage;
-    //    }
-
-    //    public string Field { get; set; }
-    //    public string ErrorMessage { get; set; }
-    //}
 }
