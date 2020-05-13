1b59e32fb785c0645dc0cb70ba197c6945cdb6e3
Martin Lush <martin-hornsby@ntlworld.com>
2020-05-13 14:30:28 +0100

    APR-1656 - Added Enums for Display Type and InputType

---
+++ src/SFA.DAS.RoatpAssessor/SFA.DAS.RoatpAssessor.Web/ApplyTypes/Enums/PageDisplayType.cs
@@ -0,0 +1,9 @@
+﻿namespace SFA.DAS.RoatpAssessor.Web.ApplyTypes.Enums
+{
+    public static class PageDisplayType
+    {
+        public const string Pages = "Pages";
+        public const string PagesWithSections = "PagesWithSections";
+        public const string OtherPagesInPagesWithSections = "OtherPagesInPagesWithSections";
+    }
+}
