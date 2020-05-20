62b415e1db411fb9c222b0323853fc479d25bf66
MarkFCain <mark.cain@digital.education.gov.uk>
2020-05-20 13:21:03 +0100

    APR-1660 corrected startingPageId

--- src/SFA.DAS.RoatpAssessor/SFA.DAS.RoatpAssessor.Web/Domain/SectionIds.cs
+++ src/SFA.DAS.RoatpAssessor/SFA.DAS.RoatpAssessor.Web/Domain/SectionIds.cs
@@ -5,7 +5,7 @@
         public static class DeliveringApprenticeshipTraining
         {
             public const int YourSectorsAndEmployees = 6;
-            public const string YourSectorsAndEmployeesStartingPageId = "7610";
+            public const string YourSectorsAndEmployeesStartingPageId = "7600";
             public const string YourSectorsAndEmployeesHeading = "Sectors and employee experience";
         }
     }
