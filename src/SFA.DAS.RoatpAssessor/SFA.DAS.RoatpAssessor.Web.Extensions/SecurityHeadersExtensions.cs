5748bd3d0d55dc48bc54fcd5d104be055da93051
gdbarnes <gregdbarnes@gmail.com>
2020-05-27 19:45:27 +0100

    Security policy changes, responsive tables, alignment and widths

--- src/SFA.DAS.RoatpAssessor/SFA.DAS.RoatpAssessor.Web.Extensions/SecurityHeadersExtensions.cs
+++ src/SFA.DAS.RoatpAssessor/SFA.DAS.RoatpAssessor.Web.Extensions/SecurityHeadersExtensions.cs
@@ -13,7 +13,15 @@ namespace SFA.DAS.RoatpAssessor.Web.Extensions
                 context.Response.Headers["X-Frame-Options"] = "SAMEORIGIN";
                 context.Response.Headers["X-XSS-Protection"] = "1; mode=block";
                 context.Response.Headers["X-Content-Type-Options"] = "nosniff";
-                context.Response.Headers["Content-Security-Policy"] = "default-src 'self'; img-src 'self' *.azureedge.net *.google-analytics.com; script-src 'self' 'unsafe-inline' *.azureedge.net *.googletagmanager.com *.postcodeanywhere.co.uk *.google-analytics.com *.googleapis.com; style-src-elem 'self' *.azureedge.net; font-src 'self' *.azureedge.net data:;";
+                context.Response.Headers["Content-Security-Policy"] =
+                    "default-src 'self'; " +
+                    "style-src 'self' 'unsafe-inline' https://*.azureedge.net; " +
+                    "img-src 'self' https://*.azureedge.net *.google-analytics.com; " +
+                    "script-src 'self' 'unsafe-inline' " +
+                        "https://das-prd-frnt-end.azureedge.net https://das-demo-frnt-end.azureedge.net https://das-pp-frnt-end.azureedge.net https://das-test-frnt-end.azureedge.net https://das-at-frnt-end.azureedge.net " +
+                        "*.googletagmanager.com *.postcodeanywhere.co.uk *.google-analytics.com *.googleapis.com; " +
+                    "style-src-elem 'self' *.azureedge.net; " +
+                    "font-src 'self' *.azureedge.net data:;";
                 context.Response.Headers["Referrer-Policy"] = "strict-origin";
                 context.Response.Headers["Cache-Control"] = "no-cache, no-store, must-revalidate";
                 context.Response.Headers["Pragma"] = "no-cache";
