0905ad5eea67d8343f86be37cad5beea32294edf
Martin Lush <martin-hornsby@ntlworld.com>
2020-08-10 13:28:20 +0100

    APR-1710 - Show correct status for all sections

--- src/SFA.DAS.RoatpAssessor/SFA.DAS.RoatpAssessor.Web/ApplyTypes/AssessorSectionStatus.cs
+++ src/SFA.DAS.RoatpAssessor/SFA.DAS.RoatpAssessor.Web/ApplyTypes/AssessorSectionStatus.cs
@@ -5,7 +5,8 @@
         public const string InProgress = "In Progress";
         public const string Pass = "Pass";
         public const string Fail = "Fail";
-        public const string FailOutOf = "fail out of";
+        public const string FailOutOf = "Fail Out Of";
+        public const string FailsOutOf = "Fails Out Of";
         public const string NotRequired = "Not Required";
 
         public const string Unknown = "Unknown";
