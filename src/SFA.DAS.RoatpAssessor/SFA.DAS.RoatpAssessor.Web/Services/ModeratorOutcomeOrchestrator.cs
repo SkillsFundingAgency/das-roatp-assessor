d610a46c13aaeb1373fce0cca9e9da1a5d03ae54
MarkFCain <mark.cain@digital.education.gov.uk>
2020-10-06 10:49:36 +0100

    APR-1718 stubbing added

--- src/SFA.DAS.RoatpAssessor/SFA.DAS.RoatpAssessor.Web/Services/ModeratorOutcomeOrchestrator.cs
+++ src/SFA.DAS.RoatpAssessor/SFA.DAS.RoatpAssessor.Web/Services/ModeratorOutcomeOrchestrator.cs
@@ -1,6 +1,9 @@
-﻿using System.Linq;
+﻿using System;
+using System.Linq;
 using System.Security.Cryptography.X509Certificates;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Razor.Language.Intermediate;
+using SFA.DAS.RoatpAssessor.Web.ApplyTypes.Apply;
 using SFA.DAS.RoatpAssessor.Web.ApplyTypes.Moderator;
 using SFA.DAS.RoatpAssessor.Web.Infrastructure.ApiClients;
 using SFA.DAS.RoatpAssessor.Web.Models;
@@ -21,6 +24,36 @@ namespace SFA.DAS.RoatpAssessor.Web.Services
 
         public async Task<ModeratorOutcomeViewModel> GetInModerationOutcomeViewModel(GetModeratorOutcomeRequest request)
         {
+            //STUBBED MFCMFC
+            if (request.ApplicationId.ToString()== "f9d3a1ba-af26-465a-8e39-08d865e55e42")
+            {
+                
+                ModeratorOutcomeViewModel viewModel = null;
+                var apply = new Apply();
+                apply.Id = Guid.NewGuid();
+                apply.ApplicationId = request.ApplicationId;
+                apply.OrganisationId = Guid.NewGuid();
+                apply.ApplicationStatus = "GatewayAssessed";
+                apply.ModerationStatus = "In Moderation";
+                apply.ApplyData = new ApplyData
+                {
+                    ApplyDetails = new ApplyDetails
+                    {
+                        ReferenceNumber = "1233",
+                        ProviderRoute = 1,
+                        ProviderRouteName = "Main provider",
+                        UKPRN = "12334",
+                        OrganisationName = "Marky Marks Emporium",
+                        ApplicationSubmittedOn = new DateTime(2020, 09, 30)
+                    }
+                };
+                viewModel = new ModeratorOutcomeViewModel(apply, request.UserId);
+                viewModel.PassCount = 50;
+                viewModel.FailCount = 1;
+
+                return viewModel;
+            }
+
             var application = await _applicationApiClient.GetApplication(request.ApplicationId);
             var contact = await _applicationApiClient.GetContactForApplication(request.ApplicationId);
             var sequences = await _moderationApiClient.GetModeratorSequences(request.ApplicationId);
@@ -48,6 +81,22 @@ namespace SFA.DAS.RoatpAssessor.Web.Services
             {
                 Status = request.Status, ReviewComment = request.ReviewComment
             };
+
+            // STUBBED MFCMFC
+            if (request.ApplicationId.ToString() == "f9d3a1ba-af26-465a-8e39-08d865e55e42")
+            {
+                var vm = new ModeratorOutcomeReviewViewModel();
+                
+                vm.ApplicantEmailAddress = "mark@test.com";
+                vm.ApplicationRoute = "Main provider";
+                vm.Ukprn = "12333444";
+                vm.ApplyLegalName = "Marky Mark Emporium";
+                vm.ReviewComment = request.ReviewComment;
+                vm.Status = request.Status;
+
+                return vm;
+            }
+
             var application = await _applicationApiClient.GetApplication(request.ApplicationId);
             if (application is null)
             {
