9a48b655ce020d62b22d807011a0fab2176e665b
Martin Lush <martin-hornsby@ntlworld.com>
2020-11-09 13:55:29 +0000

    APR-1945 - Show Moderator name on dashboard

--- src/SFA.DAS.RoatpAssessor/SFA.DAS.RoatpAssessor.Web/ViewModels/ModerationApplicationViewModel.cs
+++ src/SFA.DAS.RoatpAssessor/SFA.DAS.RoatpAssessor.Web/ViewModels/ModerationApplicationViewModel.cs
@@ -3,5 +3,6 @@
     public class ModerationApplicationViewModel : ApplicationViewModel
     {
         public string ModerationStatus { get; set; }
+        public string ModeratorName { get; set; }
     }
 }
