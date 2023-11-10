417729bac2264d4321b6e01393db107dcdea02db
Dan Ashton <daniel.ashton@dashkatconsulting.com>
2023-11-10 16:35:34 +0000

    FAI-998 - Refactor to not use package claims extension

--- src/SFA.DAS.RoatpAssessor/SFA.DAS.RoatpAssessor.Web/Controllers/Outcome/OutcomeSectionReviewController.cs
+++ src/SFA.DAS.RoatpAssessor/SFA.DAS.RoatpAssessor.Web/Controllers/Outcome/OutcomeSectionReviewController.cs
@@ -1,11 +1,10 @@
 ﻿using System;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Authorization;
-using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
-using SFA.DAS.AdminService.Common.Extensions;
 using SFA.DAS.RoatpAssessor.Web.ApplyTypes.Apply;
 using SFA.DAS.RoatpAssessor.Web.Domain;
+using SFA.DAS.RoatpAssessor.Web.Extensions;
 using SFA.DAS.RoatpAssessor.Web.Models;
 using SFA.DAS.RoatpAssessor.Web.Services;
 
